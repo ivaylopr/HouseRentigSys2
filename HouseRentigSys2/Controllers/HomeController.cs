fdb246a60d81ce6ccd46fcec91c028705484d61c
Ивайло Пръмов <ivaylopr@gmail.com>
2024-10-17 18:52:09 +0300

    MainMenu component added

--- HouseRentigSys2/Controllers/HomeController.cs
+++ HouseRentigSys2/Controllers/HomeController.cs
@@ -18,10 +18,7 @@ namespace HouseRentigSys2.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+       
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
