7646628cbf9f0a792c3ec8cfe004d44933c6243e
Ивайло Пръмов <ivaylopr@gmail.com>
2024-11-05 11:09:39 +0200

    House service and HouseIndexModel added

---
+++ HouseRentingSys2.Core/Contracts/House/IHouseService.cs
@@ -0,0 +1,9 @@
+﻿using HouseRentingSys2.Core.Models.Home;
+
+namespace HouseRentingSys2.Core.Contracts.House
+{
+	public interface IHouseService
+	{
+		Task<IEnumerable<HouseIndexServiceModel>> LastThreeHouses();
+	}
+}
---
+++ HouseRentingSys2.Core/Models/Home/HouseIndexServiceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HouseRentingSys2.Core.Models.Home
+{
+	public class HouseIndexServiceModel
+	{
+        public int Id { get; set; }
+        public string Title { get; set; }=string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
