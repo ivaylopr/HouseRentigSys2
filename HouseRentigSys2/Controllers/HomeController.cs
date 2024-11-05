8acd7b026dea083ddff14ed938ec405edd97fc09
Ивайло Пръмов <ivaylopr@gmail.com>
2024-11-05 11:10:28 +0200

    last three houses

--- HouseRentigSys2/Controllers/HomeController.cs
+++ HouseRentigSys2/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using HouseRentigSys2.Models;
+using HouseRentingSys2.Core.Contracts.House;
 using HouseRentingSys2.Core.Models.Home;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -8,15 +9,20 @@ namespace HouseRentigSys2.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IHouseService houseService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(
+            ILogger<HomeController> logger,
+            IHouseService _houseService)
         {
             _logger = logger;
+            houseService = _houseService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var model = new IndexViewModel();
+            var model = await houseService.LastThreeHouses();
+
             return View(model);
         }
 
