5d0e041a7a4faed6f3ffadd1043cc25d5d042b7f
Ивайло Пръмов <ivaylopr@gmail.com>
2024-11-05 11:08:58 +0200

    Repository added

---
+++ HouseRentingSys2.Infrastructure/Common/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace HouseRentingSys2.Infrastructure.Common
+{
+	public interface IRepository
+	{
+		IQueryable<T> All<T>() where T : class;
+
+		IQueryable<T> AllReadOnly<T>() where T : class;
+	}
+}
---
+++ HouseRentingSys2.Infrastructure/Common/Repository.cs
@@ -0,0 +1,30 @@
+﻿using HouseRentigSys2.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSys2.Infrastructure.Common
+{
+	public class Repository : IRepository
+	{
+		private readonly DbContext context;
+		public Repository(HouseRentingDbContext _context)
+		{
+			context = _context;
+		}
+
+		private DbSet<T> DbSet<T>() where T : class
+		{
+			return context.Set<T>();
+		}
+
+
+		public IQueryable<T> All<T>() where T : class
+		{
+			return DbSet<T>();
+		}
+
+		public IQueryable<T> AllReadOnly<T>() where T : class
+		{
+			return DbSet<T>().AsNoTracking();
+		}
+	}
+}
