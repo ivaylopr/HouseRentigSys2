95043c326b091c0b54c57fecd3c282b0d5b9ecf0
Ивайло Пръмов <ivaylopr@gmail.com>
2024-11-06 17:59:14 +0200

    Become Agent model created

---
+++ HouseRentingSys2.Core/Constants/MessageConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace HouseRentingSys2.Core.Constants
+{
+    public static class MessageConstants
+    {
+        public const string RequiredMessage = "The field is required!";
+        public const string LenghtMessage = "The field {0} must be between {2} and {1} characters long!";
+    }
+}
--- HouseRentingSys2.Core/Models/Agent/BecomeAgentFormModel.cs
+++ HouseRentingSys2.Core/Models/Agent/BecomeAgentFormModel.cs
@@ -1,6 +1,17 @@
-﻿namespace HouseRentingSys2.Core.Models.Agent
+﻿using System.ComponentModel.DataAnnotations;
+using static HouseRentingSys2.Core.Constants.MessageConstants;
+using static HouseRentingSys2.Infrastructure.Constants.DataConstants;
+
+namespace HouseRentingSys2.Core.Models.Agent
 {
 	public class BecomeAgentFormModel
 	{
-	}
+        [Required(ErrorMessage=RequiredMessage)]
+        [StringLength(AgentPhoneNumberMaxLength,
+            MinimumLength =AgentPhoneNumberMinLength,
+            ErrorMessage =LenghtMessage)]
+        [Display(Name ="Phone number")]
+        [Phone]
+        public string PhoneNumber { get; set; }=null!;
+    }
 }
