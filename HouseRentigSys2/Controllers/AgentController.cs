9893a3714be103b3bba22cff66c4b3a804d5e662
Ивайло Пръмов <ivaylopr@gmail.com>
2024-11-06 18:52:16 +0200

    Become AnAgent method

--- HouseRentigSys2/Controllers/AgentController.cs
+++ HouseRentigSys2/Controllers/AgentController.cs
@@ -1,8 +1,10 @@
-﻿using HouseRentingSys2.Core.Contracts;
+﻿using HouseRentigSys2.Attributes;
+using HouseRentingSys2.Core.Contracts;
 using HouseRentingSys2.Core.Models.Agent;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using static HouseRentingSys2.Core.Constants.MessageConstants;
 
 namespace HouseRentigSys2.Controllers
 {
@@ -15,20 +17,34 @@ namespace HouseRentigSys2.Controllers
             agentService = _agentService;	
         }
         [HttpGet]
-		public async Task<IActionResult> Become()
+		[NotAnAgent]
+		public IActionResult Become()
 		{
-			if (await agentService.ExistsByIdAsync(User.Id()))
-			{
-				return BadRequest();
-			}
-
+			
 			var model = new BecomeAgentFormModel();
 			return View(model);
 		}
 
 		[HttpPost]
+		[NotAnAgent]
 		public async Task<IActionResult> Become(BecomeAgentFormModel model)
 		{
+			if (await agentService.UserWithPhoneNumberExistAsync(User.Id()))
+			{
+				ModelState.AddModelError(nameof(model.PhoneNumber),PhoneExists);
+			}
+
+			if (await agentService.UserHasRentAsync(User.Id()))
+			{
+				ModelState.AddModelError("Error", HasRents);
+			}
+
+			if (ModelState.IsValid== false)
+			{
+				return View(model);
+			}
+
+			await agentService.CreateAsync(User.Id(), model.PhoneNumber);
 			return RedirectToAction(nameof(HouseController.All), "House");
 		}
 	}
--- HouseRentingSys2.Core/Constants/MessageConstants.cs
+++ HouseRentingSys2.Core/Constants/MessageConstants.cs
@@ -4,5 +4,7 @@
     {
         public const string RequiredMessage = "The field is required!";
         public const string LenghtMessage = "The field {0} must be between {2} and {1} characters long!";
+        public const string PhoneExists = "The phone number already exists, enter another one!";
+        public const string HasRents = "You should have no rents to become an agent!";
     }
 }
